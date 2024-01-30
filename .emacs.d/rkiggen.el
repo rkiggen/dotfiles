0edfbed0d7ae95391d5ec4859b8820596e154f16
rkiggen <rob.kiggen@gmail.com>
2024-01-30 19:08:23 +0100

    Added basic templating/snippet possibilities via tempEl

--- .emacs.d/rkiggen.el
+++ .emacs.d/rkiggen.el
@@ -123,6 +123,52 @@
   (setq evil-want-integration t)
   (evil-collection-init))
 
+;; Configure Tempel
+(use-package tempel
+  ;; Require trigger prefix before template name when completing.
+  ;; :custom
+  ;; (tempel-trigger-prefix "<")
+
+  :bind (("M-+" . tempel-complete) ;; Alternative tempel-expand
+	 ("M-*" . tempel-insert))
+  :config
+  ;; configure the template path
+  (setq tempel-path `("~/.emacs.d/templates/tempel/*.eld"))
+
+  :init
+
+  ;; Setup completion at point
+  (defun tempel-setup-capf ()
+    ;; Add the Tempel Capf to `completion-at-point-functions'.
+    ;; `tempel-expand' only triggers on exact matches. Alternatively use
+    ;; `tempel-complete' if you want to see all matches, but then you
+    ;; should also configure `tempel-trigger-prefix', such that Tempel
+    ;; does not trigger too often when you don't expect it. NOTE: We add
+    ;; `tempel-expand' *before* the main programming mode Capf, such
+    ;; that it will be tried first.
+    (setq-local completion-at-point-functions
+		(cons #'tempel-expand
+		      completion-at-point-functions)))
+
+  (add-hook 'conf-mode-hook 'tempel-setup-capf)
+  (add-hook 'prog-mode-hook 'tempel-setup-capf)
+  (add-hook 'text-mode-hook 'tempel-setup-capf)
+
+  ;; Optionally make the Tempel templates available to Abbrev,
+  ;; either locally or globally. `expand-abbrev' is bound to C-x '.
+  ;; (add-hook 'prog-mode-hook #'tempel-abbrev-mode)
+  ;; (global-tempel-abbrev-mode)
+)
+
+;; Optional: Add tempel-collection.
+;; The package is young and doesn't have comprehensive coverage.
+;(use-package tempel-collection)
+
+;; Optional: Use the Corfu completion UI
+;;(use-package corfu
+;;  :init
+;;  (global-corfu-mode))
+
 ; Reveal.js + Org mode
 (use-package ox-reveal
 :ensure ox-reveal)
